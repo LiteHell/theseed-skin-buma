7fcb0fa8b66f2832a3d68e0552258c2c0a6fcf88
LiteHell <LiteHell@protonmail.com>
2017-12-24 03:11:25 +0900

    expandable navbar dropdown items in mobile

--- static/js/layout.js
+++ static/js/layout.js
@@ -58,5 +58,24 @@ $(function(){
 			}
 		}
 	});
+	/* expandable navbar-dropdown items */
+	/* from https://github.com/jgthms/bulma/issues/1218 */
+	function mobile_expandable_menu() {
+		if( $(window).width() < 768 ) {
+			$('.navbar-link').next('.navbar-dropdown').hide();
+			$('.navbar-link').on('click', function(){
+				$(this).next('.navbar-dropdown').slideToggle();
+			});
+		} else {
+			$('.navbar-link').next('.navbar-dropdown').css('display','');
+			$('.navbar-link').unbind('click');
+		}
+	}
+	var screen_resize_timout;
+	$(window).on("resize", function (e) {
+		clearTimeout(screen_resize_timout);
+		screen_resize_timout = setTimeout(mobile_expandable_menu, 500);
+	});
+	mobile_expandable_menu();
     processWikiImages(false);
 })
