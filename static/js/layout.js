b9b609c9a359270dd9b10ee75ec8f8489f794129
LiteHell <LiteHell@protonmail.com>
2018-01-01 00:07:42 +0900

    fix typo

--- static/js/layout.js
+++ static/js/layout.js
@@ -64,19 +64,19 @@ function processSkinSettings() {
 			$('.wiki-article').dblclick(function (evt) {
 				var topmost = $(evt.target);
 				var headingContent = topmost.parent('.wiki-heading-content');
-				if (headingContent.length != 0) {
+				if (headingContent.length !== 0) {
 					var heading = headingContent.prev('.wiki-heading')[0];
-					document.href = heading.querySelector('.wiki-edit-section a').href;
+					location.href = heading.querySelector('.wiki-edit-section a').href;
 				} else {
 					if($(".edit-anchor").length === 0) return;
-					document.href = $(".edit-anchor")[0].href;
+					location.href = $(".edit-anchor")[0].href;
 				}
 			});
 			break;
 		case 'history':
-			$('.wiki-article').dblclick(function (evt) {
+			$('.wiki-article').dblclick(function () {
 				if ($(".history-anchor").length === 0) return;
-				document.href = $(".history-anchor")[0].href;
+				location.href = $(".history-anchor")[0].href;
 			});
 			break;
 		default:
