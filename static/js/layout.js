de082c8b1dadfdeff0a7453ff0b539812a12ac21
LiteHell <litehell@litehell.info>
2018-09-28 15:26:35 +0900

    Improved dark mode and dropdown
     * fixed a bug about navbar dropdown
     * improved dark mode

--- static/js/layout.js
+++ static/js/layout.js
@@ -166,11 +166,11 @@ $(function(){
 	/* expandable navbar-dropdown items */
 	/* from https://github.com/jgthms/bulma/issues/1218 */
 	$('.navbar-link').on('click', function(){
-		if ($(window).width() < 768 )
+		if ($(window).width() < 1024 )
 			$(this).next('.navbar-dropdown').toggle();
 	});
 	function mobile_expandable_menu() {
-		if( $(window).width() < 768 ) {
+		if( $(window).width() < 1024 ) {
 			$('.navbar-link').next('.navbar-dropdown').hide();
 		} else {
 			$('.navbar-link').next('.navbar-dropdown').css('display','');
@@ -205,5 +205,7 @@ $(function(){
 	$("#toggle-dark-theme").click(function(evt){
 		evt.preventDefault();
 		$("body").toggleClass("dark-buma");
+		sessionStorage.setItem('dark_mode', $("body").hasClass("dark-buma") ? "true" : "false");
 	});
+	$("body").toggleClass("dark-buma", JSON.parse(sessionStorage.getItem("dark_mode")) || false)
 });
