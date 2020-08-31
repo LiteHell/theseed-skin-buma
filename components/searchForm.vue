5e4dfdb7343a9b2d7b4faa548ad796828e2272c8
LiteHell <litehell@litehell.info>
2020-08-31 09:10:05 +0900

    Fix broken style of searchForm

--- components/searchForm.vue
+++ components/searchForm.vue
@@ -2,52 +2,56 @@
 
 <template>
     <form v-on:submit.prevent>
-        <div class="field has-addons">
-            <div class="control has-icons-left">
-                <input
-                    class="input is-primary"
-                    v-on:input="searchText = $event.target.value"
-                    type="text"
-                    autocomplete="off"
-                    @blur="blur"
-                    @focus="focus"
-                    @input="inputChange"
-                    @keyup.enter="keyEnter"
-                    @keydown.tab="keyEnter"
-                    @keydown.up="keyUp"
-                    @keydown.down="keyDown"
-                    placeholder="검색"
-                />
-                <span class="icon is-small is-left">
-                    <i class="fas fa-search"></i>
-                </span>
+        <div class="dropdown is-active">
+            <div class="dropdown-trigger">
+                <div class="field has-addons">
+                    <div class="control has-icons-left">
+                        <input
+                            class="input is-primary"
+                            v-on:input="searchText = $event.target.value"
+                            type="text"
+                            autocomplete="off"
+                            @blur="blur"
+                            @focus="focus"
+                            @input="inputChange"
+                            @keyup.enter="keyEnter"
+                            @keydown.tab="keyEnter"
+                            @keydown.up="keyUp"
+                            @keydown.down="keyDown"
+                            placeholder="검색"
+                        />
+                        <span class="icon is-small is-left">
+                            <i class="fas fa-search"></i>
+                        </span>
+                    </div>
+                    <div class="control">
+                        <a href="#" class="button" @click.prevent="gotodoc">
+                            <span class="icon">
+                                <i class="fas fa-search"></i>
+                            </span>
+                        </a>
+                    </div>
+                    <div class="control">
+                        <a href="#" class="button" @click.prevent="random">
+                            <span class="icon">
+                                <i class="fas fa-random"></i>
+                            </span>
+                        </a>
+                    </div>
+                </div>
             </div>
-            <div class="control">
-                <a href="#" class="button" @click.prevent="gotodoc">
-                    <span class="icon">
-                        <i class="fas fa-search"></i>
-                    </span>
+            <div class="dropdown-content" v-if="show">
+                <a
+                    class="dropdown-item"
+                    v-for="(item, i) in internalItems"
+                    @click="onClickItem(item)"
+                    v-bind:key="i"
+                    :class="{ 'is-active': i === cursor }"
+                    @mouseover="cursor = i"
+                    href="#"
+                >
+                    {{ item }}
                 </a>
-            </div>
-            <div class="control">
-                <a href="#" class="button" @click.prevent="random">
-                    <span class="icon">
-                        <i class="fas fa-random"></i>
-                    </span>
-                </a>
-            </div>
-        </div>
-
-        <div v-if="show" class="v-autocomplete-list">
-            <div
-                class="v-autocomplete-list-item"
-                v-for="(item, i) in internalItems"
-                @click="onClickItem(item)"
-                v-bind:key="i"
-                :class="{ 'v-autocomplete-item-active': i === cursor }"
-                @mouseover="cursor = i"
-            >
-                <div>{{ item }}</div>
             </div>
         </div>
     </form>
@@ -70,36 +74,3 @@ export default {
     }
 };
 </script>
-
-<style scoped>
-.v-autocomplete-list {
-    position: absolute;
-    z-index: 3;
-    border: 1px solid #ccc;
-    background-color: #fff;
-    width: 10.8rem;
-}
-@media (max-width: 1023px) {
-    .v-autocomplete-list {
-        width: 100%;
-    }
-}
-.theseed-dark-mode .v-autocomplete-list {
-    background-color: #2d2f34;
-    border: 1px solid #383b40;
-}
-.v-autocomplete-list-item {
-    cursor: pointer;
-    color: #373a3c;
-    padding: 0.5rem;
-}
-.theseed-dark-mode .v-autocomplete-list-item {
-    color: #ddd;
-}
-.v-autocomplete-list-item.v-autocomplete-item-active {
-    background-color: #f3f6fa;
-}
-.theseed-dark-mode .v-autocomplete-list-item.v-autocomplete-item-active {
-    background-color: #383b40;
-}
-</style>
