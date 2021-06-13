c3b2005d69f823476d3e57e699d5fbc35fc81522
LiteHell <litehell@litehell.info>
2021-06-13 16:00:40 +0900

    Move jump button css

--- components/jumpButtons.vue
+++ components/jumpButtons.vue
@@ -15,6 +15,35 @@
     </div>
 </template>
 
+<style scoped>
+.jump-buttons {
+    position: fixed;
+    bottom: 10px;
+    right: 10px;
+}
+.jump-buttons ul {
+    list-style-type: none;
+}
+.jump-buttons ul li {
+    cursor: pointer;
+    display: inline-block;
+    background: #00d1b2;
+    border: 1px solid #00d1b2;
+    font-size: 35px;
+    text-align: center;
+    color: white;
+    width: 50px;
+    height: 50px;
+    border-radius: 25px;
+}
+.jump-buttons ul li + li {
+    margin-left: 5px;
+}
+.jump-buttons ul li > * {
+    vertical-align: text-middle;
+}
+</style>
+
 <script>
 export default {
     methods: {
