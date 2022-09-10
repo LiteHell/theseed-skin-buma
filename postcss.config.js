1c7f7a1ec4f79056f5808ca9184251ef12f553ae
LiteHell <litehell@litehell.info>
2022-09-10 06:33:31 +0000

    Add layout.min.css build script

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,15 @@
 const fs = require('fs');
 
-module.exports = {
+module.exports = (ctx) => ({
     plugins: [
-        require("postcss-modules")({
-            getJSON: function(_cssFileName, json, _outputFileName) {
-                fs.writeFileSync('./src/bulmaClassnames.js', 'module.exports = ' + JSON.stringify(json), { encoding: 'utf8' });
-            }
-        }),
+        ctx.file.basename === 'bulma.css' &&
+            require('postcss-modules')({
+                getJSON: function (_cssFileName, json, _outputFileName) {
+                    fs.writeFileSync('./src/bulmaClassnames.js', 'module.exports = ' + JSON.stringify(json), { encoding: 'utf8' });
+                }
+            }),
         require('cssnano')({
             preset: 'default'
         })
     ]
-}
+});
