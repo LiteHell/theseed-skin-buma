a72490188d2b08b701d31dce827d5d0f26cd3708
LiteHell <litehell@litehell.info>
2022-09-10 06:41:25 +0000

    Move skin license to footer

--- layout.vue
+++ layout.vue
@@ -347,7 +347,8 @@
                         <span v-html="$store.state.config['wiki.copyright_text']" />
                         <span v-html="$store.state.config['wiki.footer_text']" />
 
-                        theseed-skin-buma by LiteHell, the seed engine by theseed.io
+                        theseed-skin-buma by LiteHell, the seed engine by theseed.io <br>
+                        theseed-skin-buma is distributed under <a href="https://www.gnu.org/licenses/gpl-3.0.html">GNU General Public License Version 3</a>
                     </p>
                 </div>
             </div>
