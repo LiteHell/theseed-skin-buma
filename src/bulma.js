6b967b475f8927c4c899c3c9aa329c17c7ae3627
LiteHell <litehell@litehell.info>
2022-11-27 22:25:16 +0900

    Implements missing argument in bulma function
     * Fixes insivible text bug in desktop skin ui
     * Closes #61

--- src/bulma.js
+++ src/bulma.js
@@ -1,15 +1,20 @@
 import bulmaClassnames from "./bulmaClassnames";
 
-export default function (classes) {
+export default function (classes, mirror = false) {
     let modified = [];
+    let unmodifeid = [];
     if (typeof classes === 'string')
-        modified = classes.split(' ').map(i => bulmaClassnames[i] || i);
+        unmodified = classes.split(' ');
     else if (typeof classes === 'object')
-        modified = Object.entries(classes).filter(([k, v]) => Boolean(v)).map(([k, v]) => k);
+        unmodifeid = Object.entries(classes).filter(([k, v]) => Boolean(v)).map(([k, v]) => k);
     else if (typeof classes === 'array')
-        modified = classes;
+        unmodifeid = classes;
     else
         throw new Error('Unexcepted parameter into bulma function')
-    modified = modified.map(i => bulmaClassnames[i] || i);
+
+    if (mirror)
+        modified = unmodified.concat(unmodified.filter(i => Object.keys(bulmaClassnames).includes(i)).map(i => bulmaClassnames[i]));
+    else
+        modified = unmodified.map(i => bulmaClassnames[i] || i);
     return modified;
 }
