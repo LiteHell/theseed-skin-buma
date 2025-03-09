c9490eba6354c288345b0a0bcec704eac709e3a7
HyeonSu Jung <unad321@naver.com>
2025-03-09 17:33:44 +0900

    Update contribution_link_edit_request

--- layout.vue
+++ layout.vue
@@ -109,6 +109,10 @@
                                 <span :class="bulma('icon')"> <font-awesome-icon icon="fas fa-chart-bar" /> </span
                                 >&nbsp; 내 토론 기여 목록
                             </nuxt-link>
+                            <nuxt-link :to="contribution_link_edit_request($store.state.session.account.uuid)" :class="bulma('navbar-item')">
+                                <span :class="bulma('icon')"> <font-awesome-icon icon="fas fa-chart-bar" /> </span
+                                >&nbsp; 내 편집 요청 기여 목록
+                            </nuxt-link>
                             <nuxt-link to="/member/starred_documents" :class="bulma('navbar-item')">
                                 <span :class="bulma('icon')"> <font-awesome-icon icon="fas fa-bookmark" /> </span
                                 >&nbsp; 별찜한 문서
