91030319d40f75d44d924dfd0517f021a3cc95f5
LiteHell <litehell@litehell.info>
2025-03-29 20:55:46 +0900

    Separate footer into another component

--- layout.vue
+++ layout.vue
@@ -342,20 +342,7 @@
                 </div>
             </div>
         </section>
-        <footer :class="bulma('footer')">
-            <div :class="bulma('container')">
-                <div :class="bulma('content has-text-centered')">
-                    <p>
-                        <span v-html="$store.state.config['wiki.copyright_text']" />
-                        <span v-html="$store.state.config['wiki.footer_text']" />
-
-                        theseed-skin-buma by LiteHell, the seed engine by theseed.io <br />
-                        theseed-skin-buma is distributed under
-                        <a href="https://www.gnu.org/licenses/gpl-3.0.html">GNU General Public License Version 3</a>
-                    </p>
-                </div>
-            </div>
-        </footer>
+        <BumaFooter />
         <jump-buttons />
     </div>
 </template>
@@ -380,6 +367,7 @@ import BDropdown from './components/b-dropdown';
 import skinLicense from './components/skinLicense';
 import bulma from './src/bulma';
 import TitleHeroBody from './components/titleHeroBody.vue';
+import BumaFooter from './components/footer.vue';
 
 library.add(fas, far);
 
@@ -393,7 +381,8 @@ export default {
         JumpButtons,
         BDropdown,
         skinLicense,
-        TitleHeroBody
+        TitleHeroBody,
+        BumaFooter
     },
     loadingBarColor(isDark) {
         return isDark ? 'white' : 'black';
