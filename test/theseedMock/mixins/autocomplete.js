7798134d7fcf4fc578fd0e93cf96358cb722a1a4
LiteHell <litehell@litehell.info>
2025-03-29 19:20:15 +0900

    Mock autocomplete mixin

--- test/theseedMock/mixins/autocomplete.js
+++ test/theseedMock/mixins/autocomplete.js
@@ -1,3 +1,20 @@
 export default {
-    
+    data() {
+        return {
+            show: true,
+            internalItems: [],
+            cursor: 0,
+            searchText: '',
+            searchTextModel: '',
+        }
+    },
+    methods: {
+        blur() {},
+        focus() {},
+        inputChange() {},
+        keyEnter() {},
+        keyUp() {},
+        keyDown() {},
+        onClickItem(item) {},
+    }
 }
